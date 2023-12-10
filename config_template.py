916f598fdf676cb54f366ecb436d9d0d7b431a18
rchakravarthy1 <rchakravarthy1@babson.edu>
2023-12-09 19:33:46 -0500

    config template

---
+++ config_template.py
@@ -0,0 +1,11 @@
+# API Keys:
+
+SPOTIFY_CLIENT_ID = "spotify_client_id"
+
+SPOTIFY_SECRET = "spotify_secret"
+
+LASTFM_API_KEY = "lastfm_api"
+
+LASFM_SHARED_SECRET = "lastfm_secret"
+
+REDIRECT_URL = "redirect_url"
